620a0ca6ee5bb849a8012fd9eff0c72aeb3a3d3f
Matthew McCracken <mccracm@mcmaster.ca>
2020-08-10 19:28:45 -0400

    testing

---
+++ src/build/precache-manifest.f5ec773d6f6b3ad9e122514f98c60437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7c72525facae5f5445093974f29d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb845b70db2a938d9df",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "0d56dd1b0e5d46a759b0",
+    "url": "/static/css/main.139658f0.chunk.css"
+  },
+  {
+    "revision": "3fb845b70db2a938d9df",
+    "url": "/static/js/2.ed059fb4.chunk.js"
+  },
+  {
+    "revision": "fb975626be61340ed45ee4bb45819288",
+    "url": "/static/js/2.ed059fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d56dd1b0e5d46a759b0",
+    "url": "/static/js/main.a6fe0946.chunk.js"
+  },
+  {
+    "revision": "e72cb63092e755704512",
+    "url": "/static/js/runtime-main.2c72f866.js"
+  }
+]);
